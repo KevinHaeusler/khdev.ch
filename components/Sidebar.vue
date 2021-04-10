eaac39d22be5e40c2353a2121db2ed39dbf3047d
KevinHaeusler <kevin.haeusler@khdev.ch>
2021-04-10 13:22:56 +0200

    more layout changes and resources

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,13 +1,112 @@
 <template>
   <div>
-    Sidebar
+    <div class="bg-stars-inverted pb-5">
+      <img
+        class="logo-image"
+        src="~/assets/resources/logo/khdev-logo-white.webp"
+        alt="KHDev Logo White"
+      />
+      <img
+        class="author-image rounded-full mx-auto my-5"
+        src="~/assets/resources/author/kevin_haeusler.webp"
+        alt="Kevin Haeusler"
+      />
+    </div>
+
+    <div class="author-description">
+      <span class="author-name">Kevin Haeusler</span>
+      <span class="author-tagline">Opensource Supporter, Hobby Gardener </span>
+      <a-divider class="author-divider" type="horizontal" />
+      <div class="author-socials">
+        <a href="mailto:mail@khdev.ch/"
+          ><a-icon type="mail" theme="filled" /> eMail |</a
+        >
+        <a
+          href="https://discordapp.com/users/120560434263621634"
+          target="_blank"
+          ><a-icon type="wechat" /> Discord |</a
+        >
+        <a href="https://github.com/KevinHaeusler/" target="_blank"
+          ><a-icon type="github" /> Github |</a
+        >
+        <a
+          href="https://www.linkedin.com/in/kevin-haeusler-3b1a12b2/"
+          target="_blank"
+          ><a-icon type="linkedin" theme="filled" /> LinkedIn
+        </a>
+      </div>
+    </div>
+
+    <nuxt-link to="/">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="inline h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path d="M12 14l9-5-9-5-9 5 9 5z" />
+        <path
+          d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+        />
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+        />
+      </svg>
+      <span class="inline">Home</span>
+    </nuxt-link>
+    <br />
+    <nuxt-link to="/archive">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="inline h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path d="M12 14l9-5-9-5-9 5 9 5z" />
+        <path
+          d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+        />
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+        />
+      </svg>
+      <span class="inline">Blog</span>
+    </nuxt-link>
+    <br />
+    <nuxt-link to="/about">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="inline h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path d="M12 14l9-5-9-5-9 5 9 5z" />
+        <path
+          d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+        />
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+        />
+      </svg>
+      <span class="inline">About</span>
+    </nuxt-link>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style></style>
