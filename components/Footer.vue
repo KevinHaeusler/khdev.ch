5e4d81aa16835d02c67c7913ec5430460f5fcb72
KevinHaeusler <kevin.haeusler@khdev.ch>
2021-04-11 09:40:20 +0200

    footer text

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,31 @@
 <template>
-  <div>Footer</div>
+  <div>
+    Kevin Haeusler@KHDev ©{{ currentDate.getFullYear() }} built with
+    <a class="hover:text-green-400 text-green-500" href="https://nuxtjs.org/"
+      >Nuxt</a
+    >
+    +
+    <a
+      class="hover:text-yellow-400 text-yellow-600"
+      href="https://content.nuxtjs.org/"
+      >Nuxt-Content</a
+    >
+    and
+    <a
+      class="hover:text-indigo-300 text-indigo-400"
+      href="https://tailwindcss.com/"
+      >TailwindCSS 2</a
+    >
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      currentDate: new Date()
+    }
+  }
 }
 </script>
 
