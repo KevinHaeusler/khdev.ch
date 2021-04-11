1c5a28f31ac1c38dda4c91b2e65ec367d37fc2fa
KevinHaeusler <kevin.haeusler@khdev.ch>
2021-04-11 10:00:58 +0200

    fix for static site mobile view maybe

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,6 +37,10 @@ export default {
     '@nuxtjs/tailwindcss',
     '@nuxtjs/device'
   ],
+  device: {
+    defaultUserAgent:
+      'Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Mobile Safari/537.36'
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
