34e35e400d6accacc1e7d345b90fb74854459509
KevinHaeusler <kevin.haeusler@khdev.ch>
2021-04-11 09:53:36 +0200

    copied style from index to blog archive

--- pages/archive.vue
+++ pages/archive.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div class="pb-5">
     <div
-      class="mb-5 bg-white shadow md:h-80 lg:m-auto lg:my-5 lg:max-w-screen-sm xl:max-w-screen-lg"
+      class="dark:bg-tblack mb-5 3xl:max-w-screen-xl bg-white shadow md:h-80 lg:m-auto lg:mb-5 lg:max-w-screen-sm xl:max-w-screen-lg"
       v-for="article of articles"
       :key="article"
     >
@@ -12,19 +12,17 @@
         />
         <div class="p-5">
           <h1>{{ article.title }}</h1>
-          <p class="text-gray-800 font-mono">
+          <p class="text-yellow-600 font-mono">
             {{ article.description }}
           </p>
-          <p class="text-gray-800 font-mono lg:mb-5">
+          <p class="dark:text-gray-300 text-gray-800 font-mono lg:mb-5">
             Kevin Haeusler | {{ formatDate(article.createdAt) }}
           </p>
           <nuxt-content
-            class="overflow whitespace-no-wrap pb-5"
+            class="overflow whitespace-no-wrap pb-5 dark:text-gray-100"
             :document="{ body: article.excerpt }"
           />
-          <button
-            class="float-left px-4 py-2 text-white font-bold bg-blue-500 hover:bg-blue-700 rounded"
-          >
+          <button class="float-left px-4 py-2 text-white bg-purple-900 shadow">
             Read more
           </button>
           <p class="float-right">Tags</p>
