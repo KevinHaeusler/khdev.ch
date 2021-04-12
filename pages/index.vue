afb128ec2e16252e94a13f3decaf6bae836d9675
KevinHaeusler <kevin.haeusler@khdev.ch>
2021-04-12 06:59:59 +0200

    fix darkmode text

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div
-      class="dark:bg-darkmodediv mb-5 pb-5 pt-20 px-5 w-screen 3xl:max-w-screen-xl dark:text-gray-100 bg-white shadow lg:m-auto lg:mb-5 lg:mt-5 lg:pt-5 lg:max-w-screen-sm xl:max-w-screen-lg"
+      class="dark:bg-darkmodediv mb-5 pb-5 pt-20 px-5 w-screen 3xl:max-w-screen-xl bg-white shadow lg:m-auto lg:mb-5 lg:mt-5 lg:pt-5 lg:max-w-screen-sm xl:max-w-screen-lg"
     >
       <h1>Welcome to KHDev</h1>
-      <div class="max-w-none prose prose-green ">
+      <div class="max-w-none prose prose-green dark:text-gray-100">
       <p>Thank you for visiting my website.</p>
       <p>
         This is a space for me to write down thoughts and experiences that I
